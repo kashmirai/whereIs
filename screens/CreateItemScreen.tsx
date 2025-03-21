08cf8118efefa2d3a0688f5eb9b3c6ad1e59c9b8
Kashmira Iljin <dkail001@edu.xamk.fi>
2025-03-21 14:10:24 +0200

    AddItem TextInput

--- screens/CreateItemScreen.tsx
+++ screens/CreateItemScreen.tsx
@@ -1,11 +1,28 @@
-import { Text } from "react-native"
+import { Button, Text, TextInput, View } from "react-native"
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context"
 
 export const CreateItem = () => {
     return (
-        <SafeAreaView>
-          <Text>Moi Createsta</Text>
+        <SafeAreaView className="mx-1">
+
+          <Text className="my-2 text-xl font-bold">Add a new item</Text>
+
+          <Text className="mt-4 mb-1">Name</Text>
+          <TextInput className="border border-black" placeholder="Syötä teksti"></TextInput>
+
+          <Text className="mt-4 mb-1">Description</Text>
+          <TextInput className="border border-black" placeholder="Syötä kuvaus"></TextInput>
+
+          <View className="mt-10">
+          <Button title="Add Image"/>
+          </View>
+
+          <View className="mt-3">
+          <Button title="Save"/>
+          </View>
+          
+
         </SafeAreaView>
 
     )
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { Button, Text } from "react-native"
+import { Button, StyleSheet, Text } from "react-native"
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context"
 import { ParamListBase, useNavigation } from "@react-navigation/native"
@@ -10,11 +10,24 @@ export const HomeScreen= () => {
 
     return (
         <SafeAreaView>
-        <Text className="text-3xl">Where is</Text>
-        <Button title="Add Item" onPress={() => navigation.navigate('Add Item')}/>
-        <Button title="List Item" onPress={() => navigation.navigate('List Item')}/>
+
+        <Text className="text-3xl text-center font-bold">Where is</Text>
+
+        <Button 
+        title="Add Item" 
+        onPress={() => navigation.navigate('Add Item')}
+        />
+        <Button 
+        title="List Item" 
+        onPress={() => navigation.navigate('List Item')}
+        />
+
         </SafeAreaView>
 
     )
 
 }
+
+const styles = StyleSheet.create({
+    
+});
