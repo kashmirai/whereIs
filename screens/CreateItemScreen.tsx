6552431884caa11fbd6350ec0d2c1dd78d427485
Kashmira Iljin <dkail001@edu.xamk.fi>
2025-03-27 18:09:59 +0200

    Sijainti

--- screens/CreateItemScreen.tsx
+++ screens/CreateItemScreen.tsx
@@ -1,14 +1,63 @@
-import { Button, Text, TextInput, View } from "react-native"
-import React, { useState } from 'react'
+import { Button, Text, TextInput, View, PermissionsAndroid } from "react-native"
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from "react-native-safe-area-context"
-import RNLocation from 'react-native-location'
+import Geolocation from 'react-native-geolocation-service';
+
+interface Location {
+  latitude: number;
+  longitude: number;
+}
 
 export const CreateItem = () => {
 
+  const Permission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Cool App Location Permission',
+          message:
+            'Cool App needs access to your location ' +
+            'so you can take awesome pictures.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the location');
+        getCurrentLocation();
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const [location, setLocation] = useState<Location | null>(null);  
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({latitude, longitude});
+        console.log(latitude, longitude);
+        setTiedot(prev => ({...prev, sijainti : latitude}))
+      }
+    )
+  }
+  
+
   const [tiedot, setTiedot] = React.useState({
     nimi : "",
-    kuvaus : ""
+    kuvaus : "",
+    sijainti : 0
   });
+
+  useEffect(() => {
+    
+  }, []);
 
 
     return (
@@ -24,6 +73,10 @@ export const CreateItem = () => {
 
           <View className="mt-10">
           <Button title="Add Image"/>
+          </View>
+
+          <View className="mt-10">
+          <Button title="Location" onPress={Permission}/>
           </View>
 
           <View className="mt-3">
